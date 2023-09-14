0ac467624180df6e05d7e4c21fb1e8e5e7e9a551
Arsonist Tefh <119712506+Arsonist-932@users.noreply.github.com>
2023-09-14 17:32:42 +0200

    Coorection mentorat

--- assets/script.js
+++ assets/script.js
@@ -24,25 +24,30 @@ const right = document.querySelector(".arrow_right");
 
 // Variables //
 let position = 0;
-let currentSlide = 0;
 
 // EVent Listener //
 left.addEventListener("click", () => {
   console.log("Click on the arrow left");
-  position = -1;
+  position -= 1;
+
+  if (position < 0) {
+    position = slides.length - 1;
+  }
+
   slider(position);
-  // Console du Slides
-  console.log(slides[currentSlide].tagLine);
-  console.log(slides[currentSlide].image);
+  bulletsActived(position);
 });
 
 right.addEventListener("click", () => {
   console.log("Click on the arrow right");
-  position = +1;
+  position += 1;
+
+  if (position > slides.length - 1) {
+    position = 0;
+  }
+
   slider(position);
-  // Console du Slides
-  console.log(slides[currentSlide].tagLine);
-  console.log(slides[currentSlide].image);
+  bulletsActived(position);
 });
 
 // Création des bullets pour le slide
@@ -54,8 +59,17 @@ function createDot() {
     dot.classList.add("dot");
     dots.appendChild(dot);
   }
+
+  bulletsActivedDefault(position);
 }
 createDot();
+
+// Création du bullet selected par défaut
+function bulletsActivedDefault(position) {
+  const bullets = document.querySelectorAll(".dot");
+
+  return bullets[position].classList.add("dot_selected");
+}
 
 // Liaisons des bullets au slide
 function bulletsActived() {
@@ -65,7 +79,7 @@ function bulletsActived() {
     const bulletsActive = bullets[i];
     console.log(bulletsActive);
 
-    if (i === currentSlide) {
+    if (i === position) {
       bulletsActive.classList.add("dot_selected");
     } else {
       bulletsActive.classList.remove("dot_selected");
@@ -73,29 +87,11 @@ function bulletsActived() {
   }
 }
 
-bulletsActived();
-
 function slider(position) {
-  // Variable qui permet d'incrémenter position à currentSilde au clic
-  currentSlide = currentSlide + position;
-
-  // Boucle permettant de rendre la Slide permanent
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-
-  if (currentSlide > slides.length - 1) {
-    currentSlide = 0;
-  }
-
   // Modifications de l'image du slide
   document.querySelector(".banner-img").src =
-    "assets/images/slideshow/" + slides[currentSlide].image;
+    "assets/images/slideshow/" + slides[position].image;
 
   // Modification de la Tagline de l'image
-  document.querySelector("#banner p").innerHTML = slides[currentSlide].tagLine;
-
-  // Lancement de la fonction bullets pour changement de classe
-  //a chaque slides sur les .dot
-  bulletsActived(position);
+  document.querySelector("#banner p").innerHTML = slides[position].tagLine;
 }
